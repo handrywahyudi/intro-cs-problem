e04a6930832c429504f0c70e4e57d1747fc01ef2
handry wahyudi <sihandry@gmail.com>
2021-03-06 15:16:08 +0700

    Refactoring code in remaining_balance.py.

--- Problem-Set-2/remaining_balance.py
+++ Problem-Set-2/remaining_balance.py
@@ -1,21 +1,17 @@
 def remaining_balance(balance, annual_interest_rate, monthly_payment_rate):
-    # For month 0
-    minimum_payment = balance * monthly_payment_rate
-    unpaid_balance = balance - minimum_payment
-    interest = annual_interest_rate / 12.0 * unpaid_balance
-    balance = unpaid_balance + interest
-
-    # For month 1 to 12
+    remaining = 0.0
     for month in range(12):
         minimum_payment = balance * monthly_payment_rate
         unpaid_balance = balance - minimum_payment
         interest = annual_interest_rate / 12.0 * unpaid_balance
-        print("Month " + str(month + 1) + " Remaining balance: %.2f" % balance)
         balance = unpaid_balance + interest
+        remaining = round(balance, 2)
+
+    return remaining
 
 
 if __name__ == "__main__":
-    balance = 484
+    balance = 42
     annual_interest_rate = 0.2
     monthly_payment_rate = 0.04
-    remaining_balance(balance, annual_interest_rate, monthly_payment_rate)
+    print(remaining_balance(balance, annual_interest_rate, monthly_payment_rate))
