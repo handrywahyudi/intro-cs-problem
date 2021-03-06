057282f89aa0eeb2d00f8aff8cc8bb771cca6808
handry wahyudi <sihandry@gmail.com>
2021-03-06 21:54:51 +0700

    Adding fixed_monthly_payment.py.

---
+++ Problem-Set-2/fixed_monthly_payment.py
@@ -0,0 +1,17 @@
+def fixed_monthly_payment(balance, annual_interest_rate):
+    current_balance = balance
+    fixed_payment = 10
+    monthly_interest_rate = annual_interest_rate / 12.0
+    while current_balance > 0:
+        current_balance = balance
+        for month in range(12):
+            current_balance -= fixed_payment
+            current_balance += (monthly_interest_rate * current_balance)
+        if current_balance > 0:
+            fixed_payment += 10
+
+    return fixed_payment
+
+
+if __name__ == "__main__":
+    print(fixed_monthly_payment(4773, 0.2))
