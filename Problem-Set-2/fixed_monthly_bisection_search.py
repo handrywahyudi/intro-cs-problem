34cbc90775db01c1bfece48aaca071af5388d9c3
handry wahyudi <sihandry@gmail.com>
2021-03-08 09:45:01 +0700

    Remove unused current_balance variable.

--- Problem-Set-2/fixed_monthly_bisection_search.py
+++ Problem-Set-2/fixed_monthly_bisection_search.py
@@ -2,10 +2,9 @@
 # Problem 3: https://bit.ly/2OtXsNy
 
 def fixed_monthly_payment(balance, annual_interest_rate):
-    current_balance = balance
     monthly_interest_rate = annual_interest_rate / 12
-    low_payment = current_balance / 12
-    high_payment = current_balance * (1 + monthly_interest_rate) ** 12 / 12
+    low_payment = balance / 12
+    high_payment = balance * (1 + monthly_interest_rate) ** 12 / 12
     while True:
         current_balance = balance
         fixed_payment = (high_payment + low_payment) / 2
@@ -14,10 +13,10 @@ def fixed_monthly_payment(balance, annual_interest_rate):
             current_balance = unpaid_balance * (1 + monthly_interest_rate)
         if high_payment - low_payment <= 0.01:
             break
-        elif current_balance > 0.0:
+        elif current_balance > 0.01:
             low_payment = fixed_payment
             continue
-        elif current_balance < 0.0:
+        elif current_balance < 0.01:
             high_payment = fixed_payment
             continue
         else:
@@ -26,6 +25,6 @@ def fixed_monthly_payment(balance, annual_interest_rate):
 
 
 if __name__ == "__main__":
-    balance = 999999
-    annual_interest_rate = 0.18
+    balance = 320000
+    annual_interest_rate = 0.2
     print("Lowest payment:", fixed_monthly_payment(balance, annual_interest_rate))
