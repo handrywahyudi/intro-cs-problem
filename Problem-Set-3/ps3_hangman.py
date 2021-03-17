a50fd82c80c7d52cef6f06e4c9ea8861472864ba
handry wahyudi <sihandry@gmail.com>
2021-03-17 14:24:37 +0700

    Complete of the function hangman game.

--- Problem-Set-3/ps3_hangman.py
+++ Problem-Set-3/ps3_hangman.py
@@ -83,23 +83,28 @@ def getGuessedWord(secretWord, lettersGuessed):
     letter_guessed_lower_string = ''.join(lettersGuessed).lower()
     underscore_list = []
     iteration = 0
+    has_letter = False
     while iteration < len(secret_word_lower_list):
         underscore_list.append("_ ")
         iteration += 1
 
     for letter_guessed_element in letter_guessed_lower_string:
         if letter_guessed_element in secret_word_lower_list:
-            has_index_letter = [secret_word_index for secret_word_index, element in enumerate(secret_word_lower_list) if element == letter_guessed_element]
-            #print(has_index_letter)
+            has_letter = True
+            has_index_letter = [secret_word_index for secret_word_index, element in enumerate(secret_word_lower_list) if
+                                element == letter_guessed_element]
+            # print(has_index_letter)
             for has_letter_element in has_index_letter:
                 underscore_list[int(has_letter_element)] = letter_guessed_element
-                #print(underscore_list[has_index_letter])
+                # print(underscore_list[has_index_letter])
+        else:
+            has_letter = False
 
     secret_word_string = ""
     for underscore_list_element in underscore_list:
         secret_word_string += underscore_list_element
 
-    return secret_word_string
+    return secret_word_string, has_letter
 
 
 def getAvailableLetters(lettersGuessed):
@@ -144,19 +149,57 @@ def hangman(secretWord):
     Follows the other limitations detailed in the problem write-up.
     '''
     # FILL IN YOUR CODE HERE...
+    print("Welcome to the game Hangman!")
+    secret_word_long = len(secretWord)
+    dashes = "-"
+    guesses_left = 8
+    letter_guessed = []
+    print(secretWord)
+    print("I am thinking of a word that is " + str(secret_word_long) + " letters long.")
+    while guesses_left > 0:
+        print(dashes * 15)
+        print("You have " + str(guesses_left) + " guesses left.")
+        available_letters = getAvailableLetters(letter_guessed)
+        print("Available letters: " + available_letters)
+        print("Please guess a letter: ")
+        guess_letter = str(input()).lower()
+        print(guess_letter)
+        letter_guessed.append(guess_letter)
+        guessed_word, has_letter = getGuessedWord(secretWord, letter_guessed)
+        elem_in_letter_guessed = [index for index, element in enumerate(letter_guessed) if element == guess_letter]
+        if has_letter:
+            if len(elem_in_letter_guessed) < 2:
+                print("Good guess:", guessed_word)
+                guesses_left -= 1
+            else:
+                print("Oops! You've already guessed that letter:", guessed_word)
+
+            if guessed_word == secretWord:
+                break
+        else:
+            print("Oops! That letter is not in my word:", guessed_word)
+            guesses_left -= 1
+
+    print(dashes * 15)
+    if guessed_word == secretWord:
+        print("Congratulations, you won!")
+    else:
+        print("Sorry, you ran out of guesses. The word was else.")
 
 # When you've completed your hangman function, uncomment these two lines
 # and run this file to test! (hint: you might want to pick your own
 # secretWord while you're testing)
 
-# secretWord = chooseWord(wordlist).lower()
-# hangman(secretWord)
-
 
 if __name__ == '__main__':
-    #letter_guessed = ['a', 'j', 'P', 'o', 'l', 'e', 'b', 'r', 't', 'q', 'm', 'j', 'n']
-    letter_guessed = ['a', 'c', 'e', 'h', 'x', 'z']
-    secret_word = 'apPLe'
-    #print(isWordGuessed(secret_word, letter_guessed))
-    #print(getGuessedWord(secret_word, letter_guessed))
-    print(getAvailableLetters(letter_guessed))
+    secretWord = chooseWord(wordlist).lower()
+    secret_word = "apple"
+    hangman(secret_word)
+
+# if __name__ == '__main__':
+#    letter_guessed = ['a', 'j', 'P', 'o', 'l', 'e', 'b', 'r', 't', 'q', 'm', 'j', 'n']
+#    letter_guessed = ['a', 'c', 'e', 'h', 'x', 'z']
+#    secret_word = 'apPLe'
+#    print(isWordGuessed(secret_word, letter_guessed))
+#    print(getGuessedWord(secret_word, letter_guessed))
+#    print(getAvailableLetters(letter_guessed))
